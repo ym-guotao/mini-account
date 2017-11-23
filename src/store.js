576f1dd7db0355df3a1b52d0c23a2002b0fe7446
ymguo <naturejolanthe@126.com>
2017-11-23 11:34:19 +0800

    add prettier settings

--- src/store.js
+++ src/store.js
@@ -1,7 +1,11 @@
-import { createStore } from 'redux';
-import rootReducer from './reducers';
+import { createStore } from 'redux'
+import rootReducer from './reducers'
 
-export default function configureStore(initialState) {
-  const store = createStore(rootReducer, initialState);
-  return store;
+export default function configureStore() {
+  const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+
+  return store
 }
