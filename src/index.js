c93e39d9a4f3124d8977458b08c253e21e674c83
ymguo <naturejolanthe@126.com>
2017-11-30 12:38:14 +0800

    eject create react app, hence can do CSS Modules

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import configureStore from './store';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import configureStore from './store'
 
-import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.css'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-const store = configureStore();
+const store = configureStore()
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>, 
+  </Provider>,
   document.getElementById('root')
-);
+)
 
-registerServiceWorker();
+registerServiceWorker()
